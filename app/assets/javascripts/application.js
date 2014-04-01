ba41bf366a41dae2123b2661bca191349a87830e
Marc Römer <admin@illnox.com>
2014-04-01 15:29:11 +0200

    Added js files for asset pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,23 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require i18n
 //= require i18n/translations
+//= require jquery
+//= require jquery_ujs
+//= require jquery_ui
+//= require turbolinks
+
+//= require "gsap/main-gsap.js"
+//= require "bootstrap.js"
+//= require "joinable.js"
+//= require "resizeable.js"
+//= require "neon-api.js"
+//= require "jquery.validate.min.js"
+//= require "neon-login.js"
+//= require "neon-register.js"
+//= require "jquery.inputmask.bundle.min.js"
+//= require "neon-custom.js"
+//= require "neon-demo.js"
+
 // require_tree .
