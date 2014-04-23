9c55e81f852a4ae23682b341d51a81fc75292ade
Marc Römer <roemermarc.393@gmail.com>
2014-04-23 12:09:58 +0200

    Added dish to model.

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -9,4 +9,28 @@ class Dish < ActiveRecord::Base
   translates :title, :drinks, :sidedish, :ingredients
   
   default_scope :order => "position, id"
+  
+  attr_accessor :image_should_process
+  has_attached_file :image, {
+    :styles => {
+      #:crop => ["286x286", :png],
+      #:cropped => ["311x283#", :png],
+      #:cropped_retina => ["622x566#", :png]
+      :crop => {
+        :geometry => "286x286",
+        :format => :png
+      },
+      :cropped => {
+        :geometry => "758x506#",
+        :format => :png,
+        :processors => [:cropper]
+      },
+      :cropped_retina => {
+        :geometry => "1516x1012#",
+        :format => :png,
+        :processors => [:cropper]
+      }
+    }
+  }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
