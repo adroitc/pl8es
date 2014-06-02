8daace409af8dac4a74e863bfdfed02460c47dde
Marc Römer <roemermarc.393@gmail.com>
2014-06-02 16:56:20 +0200

    Reorganised menu model.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,17 +1,11 @@
 class Menu < ActiveRecord::Base
   belongs_to :user
-  
+  belongs_to :default_language, :class_name => "Language"
+  has_and_belongs_to_many :languages
   belongs_to :menuLabel
-  
+  belongs_to :menuColorTemplate
+  belongs_to :menuColor
   has_many :navigations
   has_many :dishes
   has_many :beverages
-  
-  has_and_belongs_to_many :languages
-  belongs_to :default_language, :class_name => "Language"
-  
-  belongs_to :menuColorTemplate
-  
-  belongs_to :menuColor
-  
 end
