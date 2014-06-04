9dbad6e373a56c1b630ccdcde6299ad7de5c4923
Marc Römer <roemermarc.393@gmail.com>
2014-06-04 13:20:34 +0200

    Changed session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", domain: ".pl8.cc"#, :expire_after => 1.month
+  Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", domain: "pl8.cc"#, :expire_after => 1.month
 elsif Rails.env.development?
-  Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", domain: ".lvh.me"#, :expire_after => 1.month
+  Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", domain: "lvh.me"#, :expire_after => 1.month
 end
