fac984ed43810b94105a2775cd801e2bddca0aca
Marc Römer <roemermarc.393@gmail.com>
2014-06-13 09:19:45 +0200

    Fixed app/dailycious controller actions.

--- app/controllers/app/dailycious_controller.rb
+++ app/controllers/app/dailycious_controller.rb
@@ -4,23 +4,20 @@ class App::DailyciousController < ApplicationController
   end
   
   def map
-    if !params.values_at(:q).include?(nil)
-      @req_locations = Location.where(["user_id IN (?)", DailyDish.find(:all, :select => "user_id", :conditions => ["display_date = (?)", Date.today.to_datetime]).map{|d| d.user_id}]).within(
-      10,
-      :origin => [
-        params[:q].split(",")[0].to_f,
-        params[:q].split(",")[1].to_f
-      ]
-      ).order("distance")
-      
-      render :partial => "map"
-      return
-    end
-    render :json => {:status => "invalid"}
+    @req_locations = Location.where(["user_id IN (?)", DailyDish.find(:all, :select => "user_id", :conditions => ["display_date = (?)", Date.today.to_datetime]).map{|d| d.user_id}]).within(
+    10,
+    :origin => [
+      params[:lat_long].split(",")[0].to_f,
+      params[:lat_long].split(",")[1].to_f
+    ]
+    ).order("distance")
+    
+    render :partial => "map"
+    return
   end
   
   def user
-    if !params.values_at(:user_id).include?(nil) && Location.exists?(params[:user_id])
+    if Location.exists?(params[:user_id])
       @req_location = Location.find(params[:user_id])
       
       render :partial => "user"
