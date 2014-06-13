7b53e9ffa0c30e58ec53676b80971200123cf49c
Marc Römer <roemermarc.393@gmail.com>
2014-06-13 11:39:43 +0200

    Added bounds to location request.

--- app/controllers/app/dailycious_controller.rb
+++ app/controllers/app/dailycious_controller.rb
@@ -5,9 +5,18 @@ class App::DailyciousController < ApplicationController
   
   def map
     if !params.values_at(:q).include?(nil)
-      @req_locations = Location.where(["user_id IN (?)", DailyDish.find(:all, :select => "user_id", :conditions => ["display_date = (?)", Date.today.to_datetime]).map{|d| d.user_id}]).within(
-        10,
-        :origin => [
+      @req_locations = Location.where(["user_id IN (?)", DailyDish.find(:all, :select => "user_id", :conditions => ["display_date = (?)", Date.today.to_datetime]).map{|d| d.user_id}]).geo_scope(
+        :bounds => [
+          [
+            params[:q].split(",")[2].to_f,
+            params[:q].split(",")[3].to_f
+          ],
+          [
+            params[:q].split(",")[4].to_f,
+            params[:q].split(",")[5].to_f
+            ]
+          ],
+          :origin => [
           params[:q].split(",")[0].to_f,
           params[:q].split(",")[1].to_f
         ]
