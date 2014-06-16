78f9ecc77552fc43f76797cfc345c8a53ea95c2f
Marc Römer <roemermarc.393@gmail.com>
2014-06-16 16:03:53 +0200

    Added mysql escape string.

--- app/controllers/app/dailycious_controller.rb
+++ app/controllers/app/dailycious_controller.rb
@@ -61,7 +61,7 @@ class App::DailyciousController < ApplicationController
       #  ActiveRecord::Base.connection.execute("SELECT title FROM categories_users, category_translations WHERE categories_users.category_id = category_translations.category_id AND LOWER(category_translations.title) LIKE ('%#{params[:q].gsub("+"," ").downcase}%')").map{|u| u["title"]}
       #).uniq
       
-      query = params[:q].gsub("+"," ").downcase#Mysql.escape_string(params[:q].gsub("+"," ").downcase)
+      query = Mysql.escape_string(params[:q].gsub("+"," ").downcase)
       
       suggestions = DailyDish.unscoped.find(
         :all,
@@ -97,7 +97,7 @@ class App::DailyciousController < ApplicationController
   
   def search
     if !params.values_at(:q).include?(nil)
-      query = params[:q].gsub("+"," ").downcase#Mysql.escape_string(params[:q].gsub("+"," ").downcase)
+      query = Mysql.escape_string(params[:q].gsub("+"," ").downcase)
       
       @req_locations = Location.where(
         ["user_id IN (?)",
