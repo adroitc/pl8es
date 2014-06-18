d4e2a6bf27985fad7e605849616a3e744138a6b6
Marc Römer <roemermarc.393@gmail.com>
2014-06-18 10:54:27 +0200

    31 days cookie expiration date.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session"#, :expire_after => 1.month
-#if Rails.env.production?
-#  Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", domain: "pl8.cc"
-#elsif Rails.env.development?
-#  Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", domain: "lvh.me"
-#end
+Pl8es::Application.config.session_store :cookie_store, key: "_pl8es_session", :expire_after => 31.days
