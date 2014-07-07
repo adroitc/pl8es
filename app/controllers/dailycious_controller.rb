bb907f6903677c84f561611595e281945df676c3
Marc Römer <roemermarc.393@gmail.com>
2014-07-07 12:17:15 +0200

    Fixed 0 dailcious week.

--- app/controllers/dailycious_controller.rb
+++ app/controllers/dailycious_controller.rb
@@ -2,7 +2,7 @@ class DailyciousController < ApplicationController
   
   def index
     if params[:add_weeks] == "0"
-      redirect_to :controller => "dailycious", :action => "index"
+      redirect_to :controller => "dailycious", :action => "index", :add_weeks => nil
     elsif !@user
       redirect_to :controller => "login", :action => "index"
     end
