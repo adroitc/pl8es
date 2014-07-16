a385aa870b84e28ab627779cff66d2b705ff8ceb
Marc Römer <roemermarc.393@gmail.com>
2014-07-16 19:32:05 +0200

    Removed last columns from users.

---
+++ db/migrate/20140716172735_remove_old_user_image_columns.rb
@@ -0,0 +1,29 @@
+class RemoveOldUserImageColumns < ActiveRecord::Migration
+  def change
+    remove_column :users, :restaurant_image_file_name, :string
+    remove_column :users, :restaurant_image_content_type
+    remove_column :users, :restaurant_image_file_size, :integer
+    remove_column :users, :restaurant_image_updated_at, :datetime
+    remove_column :users, :restaurant_image_dimensions, :string
+    
+    remove_column :users, :logo_image_file_name, :string
+    remove_column :users, :logo_image_content_type, :string
+    remove_column :users, :logo_image_file_size, :integer
+    remove_column :users, :logo_image_updated_at, :datetime
+    remove_column :users, :logo_image_dimensions, :string
+    
+    remove_column :users, :appmain_image_file_name, :string
+    remove_column :users, :appmain_image_content_type, :string
+    remove_column :users, :appmain_image_file_size, :integer
+    remove_column :users, :appmain_image_updated_at, :datetime
+    remove_column :users, :appmain_image_dimensions, :string
+    
+    remove_column :users, :splashscreen_image_file_name, :string
+    remove_column :users, :splashscreen_image_content_type, :string
+    remove_column :users, :splashscreen_image_file_size, :integer
+    remove_column :users, :splashscreen_image_updated_at, :datetime
+    remove_column :users, :splashscreen_image_dimensions, :string
+    remove_column :users, :
+    remove_column :users, :splashscreen_image_processed, :boolean
+  end
+end
