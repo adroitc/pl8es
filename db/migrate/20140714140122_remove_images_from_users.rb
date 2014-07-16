c7e4b5a5c127334f80521359aa196e037e107c77
Marc Römer <roemermarc.393@gmail.com>
2014-07-16 18:47:03 +0200

    Fix migration error.

--- db/migrate/20140714140122_remove_images_from_users.rb
+++ db/migrate/20140714140122_remove_images_from_users.rb
@@ -1,43 +1,43 @@
 class RemoveImagesFromUsers < ActiveRecord::Migration
   def change
     #remove_attachment :users, :preview_image
-    remove_column :users, :preview_image_fingerprint, :string
-    remove_column :users, :preview_image_crop_w, :integer
-    remove_column :users, :preview_image_crop_h, :integer
-    remove_column :users, :preview_image_crop_x, :integer
-    remove_column :users, :preview_image_crop_y, :integer
-    remove_column :users, :preview_image_processed, :boolean, :default => true
-    
-    remove_attachment :users, :appmain_image
-    remove_column :users, :appmain_image_fingerprint, :string
-    remove_column :users, :appmain_image_crop_w, :integer
-    remove_column :users, :appmain_image_crop_h, :integer
-    remove_column :users, :appmain_image_crop_x, :integer
-    remove_column :users, :appmain_image_crop_y, :integer
-    remove_column :users, :appmain_image_processed, :boolean, :default => true
-    
-    remove_attachment :users, :logo_image
-    remove_column :users, :logo_image_fingerprint, :string
-    remove_column :users, :logo_image_crop_w, :integer
-    remove_column :users, :logo_image_crop_h, :integer
-    remove_column :users, :logo_image_crop_x, :integer
-    remove_column :users, :logo_image_crop_y, :integer
-    remove_column :users, :logo_image_processed, :boolean, :default => true
-    
-    remove_attachment :users, :splashscreen_image
-    remove_column :users, :splashscreen_image_fingerprint, :string
-    remove_column :users, :splashscreen_image_crop_w, :integer
-    remove_column :users, :splashscreen_image_crop_h, :integer
-    remove_column :users, :splashscreen_image_crop_x, :integer
-    remove_column :users, :splashscreen_image_crop_y, :integer
-    remove_column :users, :splashscreen_image_processed, :boolean, :default => true
-    
-    remove_attachment :users, :restaurant_image
-    remove_column :users, :restaurant_image_fingerprint, :string
-    remove_column :users, :restaurant_image_crop_w, :integer
-    remove_column :users, :restaurant_image_crop_h, :integer
-    remove_column :users, :restaurant_image_crop_x, :integer
-    remove_column :users, :restaurant_image_crop_y, :integer
-    remove_column :users, :restaurant_image_processed, :boolean, :default => true
+    #remove_column :users, :preview_image_fingerprint, :string
+    #remove_column :users, :preview_image_crop_w, :integer
+    #remove_column :users, :preview_image_crop_h, :integer
+    #remove_column :users, :preview_image_crop_x, :integer
+    #remove_column :users, :preview_image_crop_y, :integer
+    #remove_column :users, :preview_image_processed, :boolean, :default => true
+    #
+    #remove_attachment :users, :appmain_image
+    #remove_column :users, :appmain_image_fingerprint, :string
+    #remove_column :users, :appmain_image_crop_w, :integer
+    #remove_column :users, :appmain_image_crop_h, :integer
+    #remove_column :users, :appmain_image_crop_x, :integer
+    #remove_column :users, :appmain_image_crop_y, :integer
+    #remove_column :users, :appmain_image_processed, :boolean, :default => true
+    #
+    #remove_attachment :users, :logo_image
+    #remove_column :users, :logo_image_fingerprint, :string
+    #remove_column :users, :logo_image_crop_w, :integer
+    #remove_column :users, :logo_image_crop_h, :integer
+    #remove_column :users, :logo_image_crop_x, :integer
+    #remove_column :users, :logo_image_crop_y, :integer
+    #remove_column :users, :logo_image_processed, :boolean, :default => true
+    #
+    #remove_attachment :users, :splashscreen_image
+    #remove_column :users, :splashscreen_image_fingerprint, :string
+    #remove_column :users, :splashscreen_image_crop_w, :integer
+    #remove_column :users, :splashscreen_image_crop_h, :integer
+    #remove_column :users, :splashscreen_image_crop_x, :integer
+    #remove_column :users, :splashscreen_image_crop_y, :integer
+    #remove_column :users, :splashscreen_image_processed, :boolean, :default => true
+    #
+    #remove_attachment :users, :restaurant_image
+    #remove_column :users, :restaurant_image_fingerprint, :string
+    #remove_column :users, :restaurant_image_crop_w, :integer
+    #remove_column :users, :restaurant_image_crop_h, :integer
+    #remove_column :users, :restaurant_image_crop_x, :integer
+    #remove_column :users, :restaurant_image_crop_y, :integer
+    #remove_column :users, :restaurant_image_processed, :boolean, :default => true
   end
 end
