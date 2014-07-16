a6537e9655ae820928fcd84a61348afdcf57ba45
Marc Römer <roemermarc.393@gmail.com>
2014-07-16 19:02:09 +0200

    Fixed migration error.

--- db/migrate/20140716165733_remove_old_image_variables.rb
+++ db/migrate/20140716165733_remove_old_image_variables.rb
@@ -1,12 +1,12 @@
 class RemoveOldImageVariables < ActiveRecord::Migration
   def change
     #remove_attachment :users, :preview_image
-    remove_column :users, :preview_image_fingerprint, :string
-    remove_column :users, :preview_image_crop_w, :integer
-    remove_column :users, :preview_image_crop_h, :integer
-    remove_column :users, :preview_image_crop_x, :integer
-    remove_column :users, :preview_image_crop_y, :integer
-    remove_column :users, :preview_image_processed, :boolean, :default => true
+    #remove_column :users, :preview_image_fingerprint, :string
+    #remove_column :users, :preview_image_crop_w, :integer
+    #remove_column :users, :preview_image_crop_h, :integer
+    #remove_column :users, :preview_image_crop_x, :integer
+    #remove_column :users, :preview_image_crop_y, :integer
+    #remove_column :users, :preview_image_processed, :boolean, :default => true
     
     #remove_attachment :users, :appmain_image
     remove_column :users, :appmain_image_fingerprint, :string
