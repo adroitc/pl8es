62f27c84c57734ee613435fa139a09e84219da2b
Marc Römer <roemermarc.393@gmail.com>
2014-07-16 18:40:42 +0200

    Fix migration error.

--- db/migrate/20140714111136_add_columns_to_restaurant.rb
+++ db/migrate/20140714111136_add_columns_to_restaurant.rb
@@ -1,6 +1,6 @@
 class AddColumnsToRestaurant < ActiveRecord::Migration
   def change
-    remove_reference :users, :defaultMenu
+    #remove_reference :users, :defaultMenu
     add_reference :restaurants, :defaultMenu
 
     User.drop_translation_table!
