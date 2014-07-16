9da7d94a5425b23926a78ba4d7d49d25013179bd
Marc Römer <roemermarc.393@gmail.com>
2014-07-16 19:34:20 +0200

    Fixed last migration.

--- db/migrate/20140716172735_remove_old_user_image_columns.rb
+++ db/migrate/20140716172735_remove_old_user_image_columns.rb
@@ -1,7 +1,7 @@
 class RemoveOldUserImageColumns < ActiveRecord::Migration
   def change
     remove_column :users, :restaurant_image_file_name, :string
-    remove_column :users, :restaurant_image_content_type
+    remove_column :users, :restaurant_image_content_type, :string
     remove_column :users, :restaurant_image_file_size, :integer
     remove_column :users, :restaurant_image_updated_at, :datetime
     remove_column :users, :restaurant_image_dimensions, :string
