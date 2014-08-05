41b706f20360d123fc2edc0662292a09be9007c7
Marc Römer <roemermarc.393@gmail.com>
2014-08-05 22:13:04 +0200

    Fixed menumalist design.2

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -8,6 +8,7 @@ class AjaxController < ApplicationController
         @user.restaurant.menuColorTemplate = nil
       end
       @user.restaurant.background_type = params[:background_type]
+      @user.restaurant.save
       
       @user.restaurant.menuColor.update_attributes(
         params.permit(:background, :bar_background, :nav_text, :nav_text_active).merge({
