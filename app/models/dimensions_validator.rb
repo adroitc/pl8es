bfd6307fa306dde3b9203dc12ebd10f77c1ef83c
Marc Römer <roemermarc.393@gmail.com>
2014-08-08 12:38:43 +0200

    Finished new image crop management.

--- app/models/dimensions_validator.rb
+++ app/models/dimensions_validator.rb
@@ -6,7 +6,7 @@ class DimensionsValidator < ActiveModel::EachValidator
       width = options[:width]
       height = options[:height]
       
-      puts "width----------------------------------------"+dimensions.width.to_s+"----"+width.to_s
+      #puts "width----------------------------------------"+dimensions.width.to_s+"----"+width.to_s
       
       record.errors[attribute] << "Width must be #{width}px" unless dimensions.width >= width
       record.errors[attribute] << "Height must be #{height}px" unless dimensions.height >= height
