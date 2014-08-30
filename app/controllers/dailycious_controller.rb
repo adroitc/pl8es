a9a2b35185db89c138e8fd0ce63d2ea7dfdaeee4
Marc Römer <roemermarc.393@gmail.com>
2014-08-30 14:53:43 +0200

    Reset daylicious controller.

--- app/controllers/dailycious_controller.rb
+++ app/controllers/dailycious_controller.rb
@@ -1,9 +1,6 @@
 class DailyciousController < ApplicationController
   
   def index
-    render :json => params
-    return
-    
     if params[:add_weeks] == "0"
       redirect_to :controller => "dailycious", :action => "index", :add_weeks => nil
     elsif !@user
