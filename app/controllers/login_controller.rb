e12b9fa3d9c91edc708369cb8bd684e6650b2418
Marc Römer <roemermarc.393@gmail.com>
2014-09-09 15:27:51 +0200

    Fixe forgot password.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,22 @@ class LoginController < ApplicationController
   
   def index
     if @user
-      redirect_to :controller => "menumalist", :action => "index"
+      redirect_to :controller => "profile", :action => "index"
+    end
+  end
+  
+  def login_forgot
+    if @user
+      redirect_to :controller => "profile", :action => "index"
+    end
+  end
+  
+  def forgot_reset
+    if !params.values_at(:user_id, :reset_token).include?(nil)
+      @user = User.find(params[:user_id])
+      if !(!@user.blank? && @user.reset_token == params[:reset_token] && @user.reset_date < DateTime.now+24.hours && @user.updated_at < @user.reset_date+1.seconds)
+        raise ActionController::RoutingError.new("Not Found")
+      end
     end
   end
    
