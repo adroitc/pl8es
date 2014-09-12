761d8e23bfd3a351e34fce5914de92e30d103dc3
Marc Römer <roemermarc.393@gmail.com>
2014-09-12 11:44:47 +0200

    Fixed images.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,12 @@
 class ProfileController < ApplicationController
   
   def index
+    @user.send_mail(t("email.signup_dailycious_send"), t("email.signup_dailycious_subj"), t("email.signup_dailycious_msg",{:n=>@user.restaurant.name}))
+    
+    @user.send_mail(t("email.signup_menumalist_send"), t("email.signup_menumalist_subj"), t("email.signup_menumalist_msg",{:n=>@user.restaurant.name,:e=>@user.email,:c=>@user.restaurant.download_code}))
+    
+    @user.send_mail(t("email.signup_pl8_send"), t("email.signup_pl8_subj"), t("email.signup_pl8_msg",{:n=>@user.restaurant.name,:e=>@user.email}))
+    
     if !@user
       redirect_to :controller => "login", :action => "index"
     end
