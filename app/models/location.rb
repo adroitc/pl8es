d0087219a436b06a3df279b862b93ffabe788998
Marc Römer <marc@roemer.io>
2014-09-22 11:57:05 +0200

    Fixed address problem in dailycious app.

--- app/models/location.rb
+++ app/models/location.rb
@@ -24,30 +24,41 @@ class Location < ActiveRecord::Base
   def self.validate_address(params)
     if !params.values_at(:address, :zip, :city, :country).include?(nil)
       google_address = params[:address].gsub(" ","+")+","+params[:zip].gsub(" ","+")+","+params[:city].gsub(" ","+")+","+params[:country].gsub(" ","+")
-      google_url = URI.parse(URI.encode("http://maps.googleapis.com/maps/api/geocode/json?address="+google_address+"&sensor=false&language="+I18n.locale.to_s))
-      google_req = Net::HTTP::Get.new(google_url.request_uri)
-      google_res = Net::HTTP.start(google_url.host, google_url.port) {|http|
-        http.request(google_req)
-      }
-      google_results = JSON.parse(google_res.body)["results"]
-      if google_results.count == 1 && google_results[0]["geometry"]["location_type"] == "ROOFTOP"
+      #google_url = URI.parse(URI.encode("http://maps.googleapis.com/maps/api/geocode/json?address="+google_address+"&sensor=false&language="+I18n.locale.to_s))
+      #google_req = Net::HTTP::Get.new(google_url.request_uri)
+      #google_res = Net::HTTP.start(google_url.host, google_url.port) {|http|
+      #  http.request(google_req)
+      #}
+      #google_results = JSON.parse(google_res.body)["results"]
+      #if google_results.count == 1 && google_results[0]["geometry"]["location_type"] == "ROOFTOP"
+      #  return {
+      #    :address => google_results[0]["address_components"].find_all{|item|
+      #      item["types"] == ["route"]
+      #    }[0]["long_name"]+" "+google_results[0]["address_components"].find_all{|item|
+      #      item["types"] == ["street_number"]
+      #    }[0]["long_name"],
+      #    :zip => google_results[0]["address_components"].find_all{|item|
+      #      item["types"] == ["postal_code"]
+      #    }[0]["long_name"],
+      #    :city => google_results[0]["address_components"].find_all{|item|
+      #      item["types"] == ["locality", "political"]
+      #    }[0]["long_name"],
+      #    :country => google_results[0]["address_components"].find_all{|item|
+      #      item["types"] == ["country", "political"]
+      #    }[0]["long_name"],
+      #    :latitude => google_results[0]["geometry"]["location"]["lat"].to_f,
+      #    :longitude => google_results[0]["geometry"]["location"]["lng"].to_f
+      #  }
+      #end
+      geokit_address = Geokit::Geocoders::GoogleGeocoder.geocode(google_address)
+      if geokit_address.precision == "building"
         return {
-          :address => google_results[0]["address_components"].find_all{|item|
-            item["types"] == ["route"]
-          }[0]["long_name"]+" "+google_results[0]["address_components"].find_all{|item|
-            item["types"] == ["street_number"]
-          }[0]["long_name"],
-          :zip => google_results[0]["address_components"].find_all{|item|
-            item["types"] == ["postal_code"]
-          }[0]["long_name"],
-          :city => google_results[0]["address_components"].find_all{|item|
-            item["types"] == ["locality", "political"]
-          }[0]["long_name"],
-          :country => google_results[0]["address_components"].find_all{|item|
-            item["types"] == ["country", "political"]
-          }[0]["long_name"],
-          :latitude => google_results[0]["geometry"]["location"]["lat"].to_f,
-          :longitude => google_results[0]["geometry"]["location"]["lng"].to_f
+          :address => geokit_address.street_name+" "+geokit_address.street_number,
+          :zip => geokit_address.zip,
+          :city => geokit_address.city,
+          :country => geokit_address.country,
+          :latitude => geokit_address.ll[0],
+          :longitude => geokit_address.ll[1]
         }
       end
     end
