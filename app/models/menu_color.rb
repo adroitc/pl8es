b36a2ff84357568e40296884dd5931767944c8e6
DaveGson <dave.gson@gmail.com>
2015-01-12 15:27:34 +0100

    validation typo, small bugfix

--- app/models/menu_color.rb
+++ app/models/menu_color.rb
@@ -4,6 +4,6 @@ class MenuColor < ActiveRecord::Base
 	validates :background, :presence => true, :length => 4..7
 	validates :bar_background, :presence => true, :length => 4..7
 	validates :nav_text, :presence => true, :length => 4..7
-	validates :nav_text_active, :presence => true, :length 4..7
+	validates :nav_text_active, :presence => true, :length => 4..7
 	
 end
