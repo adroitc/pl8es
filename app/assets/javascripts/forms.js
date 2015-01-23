d7a4f3fdbbb3cda12780dee300f37dbd96715546
DaveGson <dave.gson@gmail.com>
2015-01-23 09:59:11 +0100

    rewrote delete partial of modal, cleaner js & css

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+	// –– changes button style when toggling a hidden checkbox
 	$("#default-modal").on("click", ".form-group input[type='checkbox']", function() {
 		$(this).siblings(".btn").toggleClass("btn-green btn-default");
+	});
+	
+	// –– deleting a record requires a confirm - manage hiding & showing
+	$("#default-modal").on("click", ".modal-delete button.delete", function() {
+		$(this).hide();
+		$(this).siblings(".cancel, .confirm").show();
+	});
+	$("#default-modal").on("click", ".modal-delete button.cancel", function() {
+		$(this).siblings(".delete").show();
+		$(this).hide();
+		$(this).siblings(".confirm").hide();
 	});
 });
