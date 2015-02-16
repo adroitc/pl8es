d0055fbd4c4b5e32f229f53d7ee6b33dd8f2a167
DaveGson <dave.gson@gmail.com>
2015-02-16 16:20:24 +0100

    search term needs at least 2 characters

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,13 @@
 class RestaurantsController < ApplicationController
 	
 	def search
-		search_term = params[:s].gsub(/\s+/, " ")
-		search_term = "%#{search_term.squish.downcase}%"
-		
-		@restaurants = Restaurant.referred.where("lower(restaurants.name) LIKE ?", search_term).joins(:location)
+		if params[:s].length > 1
+			search_term = params[:s].gsub(/\s+/, " ")
+			search_term = "%#{search_term.squish.downcase}%"
+			
+			@restaurants = Restaurant.referred.where("lower(restaurants.name) LIKE ?", search_term).joins(:location)
+		else
+			@restaurants = nil
+		end
 	end
 end
