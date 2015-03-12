bb6844f3c8ee4ac7a26e5fe5693592afc032a6a2
DaveGson <dave.gson@gmail.com>
2015-03-12 10:51:37 +0100

    add past & present dishes as variables in controller => cleaner view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,17 +1,18 @@
 class ProfileController < ApplicationController
-  
-  def index
-    if !@user
-      redirect_to login_index_path
-    end
-  end
-  
-  def public
-    if Restaurant.exists?(params[:restaurant_id])
-      @visit_restaurant = Restaurant.find(params[:restaurant_id])
-    else
-      raise ActionController::RoutingError.new("Not Found")
-    end
-  end
-  
+	
+	def index
+		unless @user
+			redirect_to login_index_path
+		end
+	end
+	
+	def public
+		if Restaurant.exists?(params[:restaurant_id])
+			@visit_restaurant = Restaurant.find(params[:restaurant_id])
+			@todays_daily_dishes = @restaurant.daily_dishes.where(:display_date => Date.today.to_datetime)
+			@past_daily_dishes = @restaurant.daily_dishes.where('display_date < ?', Date.today.to_datetime).order('display_date DESC')
+		else
+			raise ActionController::RoutingError.new("Not Found")
+		end
+	end
 end
