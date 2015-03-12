2f2e951e08c07f17516227e69a8ba9bd2b49d288
DaveGson <dave.gson@gmail.com>
2015-03-12 10:57:44 +0100

    small typos

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,8 +9,8 @@ class ProfileController < ApplicationController
 	def public
 		if Restaurant.exists?(params[:restaurant_id])
 			@visit_restaurant = Restaurant.find(params[:restaurant_id])
-			@todays_daily_dishes = @restaurant.daily_dishes.where(:display_date => Date.today.to_datetime)
-			@past_daily_dishes = @restaurant.daily_dishes.where('display_date < ?', Date.today.to_datetime).order('display_date DESC')
+			@todays_daily_dishes = @visit_restaurant.daily_dishes.where(:display_date => Date.today.to_datetime)
+			@past_daily_dishes = @visit_restaurant.daily_dishes.where('display_date < ?', Date.today.to_datetime).order('display_date DESC')
 		else
 			raise ActionController::RoutingError.new("Not Found")
 		end
