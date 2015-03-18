a3cd469b1917ecb4cd88065170c93f3f82f35d02
DaveGson <dave.gson@gmail.com>
2015-03-18 12:37:23 +0100

    added a second layout for devise forms
    - also restructured css files for easier readability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_filter :set_current_locale, :set_device
-
+	
+	layout :resolve_layout
+	
 	private
 	
 		def authenticate_user
@@ -15,59 +17,67 @@ class ApplicationController < ActionController::Base
 			@default_language = current_user.restaurant.default_language
 			@languages = current_user.restaurant.languages.order(:id)
 		end
-	
-	def set_device
-		#device
-		if Device.validHeader(request.headers)
-			device_content = {
-				:user => current_user,
-				:device_id => request.headers["Device-Id"],
-				:device_app => request.headers["Device-App"],
-				:device_version => request.headers["Device-Version"],
-				:device_type => request.headers["Device-Type"],
-				:device_system => request.headers["Device-System"]
-			}
-			if Device.exists?(:device_id => request.headers["Device-Id"])
-				@device = Device.find_by_device_id(request.headers["Device-Id"])
+		
+		def resolve_layout
+			if devise_controller?
+				'devise'
 			else
-				@device = Device.create()
+				'application'
 			end
-			@device.update_attributes(device_content)
-			if @session
-				@session.update_attributes({
+		end
+	
+		def set_device
+			#device
+			if Device.validHeader(request.headers)
+				device_content = {
 					:user => current_user,
-					:device => @device
-				})
+					:device_id => request.headers["Device-Id"],
+					:device_app => request.headers["Device-App"],
+					:device_version => request.headers["Device-Version"],
+					:device_type => request.headers["Device-Type"],
+					:device_system => request.headers["Device-System"]
+				}
+				if Device.exists?(:device_id => request.headers["Device-Id"])
+					@device = Device.find_by_device_id(request.headers["Device-Id"])
+				else
+					@device = Device.create()
+				end
+				@device.update_attributes(device_content)
+				if @session
+					@session.update_attributes({
+						:user => current_user,
+						:device => @device
+					})
+				end
+				@device.touch
 			end
-			@device.touch
 		end
-	end
-	
-	def set_current_locale
-		#language
-		if params[:locale]
-			I18n.locale = params[:locale]
-			if current_user && Language.exists?(:locale => params[:locale])
-				current_user.restaurant.update_attributes(:default_language => Language.find(:locale => params[:locale]))
+		
+		def set_current_locale
+			#language
+			if params[:locale]
+				I18n.locale = params[:locale]
+				if current_user && Language.exists?(:locale => params[:locale])
+					current_user.restaurant.update_attributes(:default_language => Language.find(:locale => params[:locale]))
+				end
+				if session[:signup]
+					session[:signup][:locale] = params[:locale]
+				end
+			elsif !current_user && extract_locale_from_accept_language_header
+				I18n.locale = extract_locale_from_accept_language_header
+				if session[:signup]
+					session[:signup][:locale] = extract_locale_from_accept_language_header
+				end
+			elsif current_user
+				I18n.locale = current_user.restaurant.default_language.locale
 			end
-			if session[:signup]
-				session[:signup][:locale] = params[:locale]
-			end
-		elsif !current_user && extract_locale_from_accept_language_header
-			I18n.locale = extract_locale_from_accept_language_header
-			if session[:signup]
-				session[:signup][:locale] = extract_locale_from_accept_language_header
-			end
-		elsif current_user
-			I18n.locale = current_user.restaurant.default_language.locale
 		end
-	end
-	
-	def extract_locale_from_accept_language_header
-		if request.env['HTTP_ACCEPT_LANGUAGE']
-			return request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+		
+		def extract_locale_from_accept_language_header
+			if request.env['HTTP_ACCEPT_LANGUAGE']
+				return request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+			end
+			nil
 		end
-		nil
-	end
 	
 end
