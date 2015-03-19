68213f30c817ddf24fe46565d721ecf4a8b347be
davegson <dave.gson@gmail.com>
2015-03-19 13:58:50 +0100

    added further basic user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,18 @@ describe User do
 		before { @user.email = nil }
 		it { should_not be_valid }
 	end
+	
+	describe "when password is not present" do
+		before { @user.password = nil }
+		it { should_not be_valid }
+	end
+	
+	describe "when email & password are present" do
+		it { should be_valid }
+	end
+	
+	it "should have encrypted password after save" do
+		@user.save
+		expect(@user.encrypted_password).to be_truthy
+	end
 end
