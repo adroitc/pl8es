2c3245300c05f20648932379e2631afb0e795e30
davegson <dave.gson@gmail.com>
2015-03-19 15:56:54 +0100

    removed unused session / user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,11 @@ class User < ActiveRecord::Base
 	
 	has_many :authentications
 	
-  has_one :restaurant
-  has_many :devices
-  has_many :sessions
+	has_one :restaurant
+	has_many :devices
+	has_many :sessions
 	
-	def self.loggedIn(session)
-		if session[:user_id] && User.exists?(session[:user_id])
-			return true
-		end
-		return false
-	end
-  
-  def send_mail(sender, subject, content)
+	def send_mail(sender, subject, content)
     RestClient.post "https://api:#{ENV["MAILGUN_API"]}"\
     "@api.mailgun.net/v2/pl8.cc/messages",
       :from => "#{sender} <hi@pl8.cc>",
