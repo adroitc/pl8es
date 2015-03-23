b3edd3fe5f2d3f33e97064ae160f435ba5ada64a
davegson <dave.gson@gmail.com>
2015-03-23 10:40:41 +0100

    remove #_=_ from the path - facebook omniauth

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -176,6 +176,12 @@ $(document).ready(function()
       }
     });
   });
+	
+	if (window.location.hash == '#_=_'){
+		history.replaceState 
+			? history.replaceState(null, null, window.location.href.split('#')[0])
+			: window.location.hash = '';
+	}
 });
 function pl8es_i_ajaxform(f,a)
 {
