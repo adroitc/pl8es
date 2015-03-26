02a4488e5dca8ac54489a3c5b56652a5c2ebb836
davegson <dave.gson@gmail.com>
2015-03-26 15:35:58 +0100

    configuration changes to avoid future deprecations

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Pl8es
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.active_record.raise_in_transactional_callbacks = true
     
     # scan model subfolders, makes structuring models easy
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
