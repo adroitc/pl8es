e46f182d1207385f1f9ec0f4969f5c589ca834a3
davegson <dave.gson@gmail.com>
2015-04-10 14:48:49 +0200

    removed test_help as it lead to an unwanted error message
    http://stackoverflow.com/questions/23045992/rspec-guard-running-minitest
    -and-getting-invalid-option-f

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require 'rails/test_help'
 require 'rspec/rails'
 require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+# Checks for pending migrations before tests are run.
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 	config.expect_with(:rspec) { |c| c.syntax = :expect }
