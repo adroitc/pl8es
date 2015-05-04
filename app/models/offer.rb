f673b4914943b6d714eea5f36643eebe9fc37954
davegson <dave.gson@gmail.com>
2015-05-04 15:00:01 +0200

    added in_range scope
    gets all offers that have at least one offer in the given time range

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,5 +9,6 @@ class Offer < ActiveRecord::Base
 	
 	def dates(options={})
 		Recurrence.new({:every => every, :on => on, :interval => interval, :repeat => repeat, :starts => start_date, :until => end_date}.merge(options)).events
+	scope :in_range, ->(range) { where("end_date >= ?", range.first).where("start_date <= ?", range.last) }
 	end
 end
