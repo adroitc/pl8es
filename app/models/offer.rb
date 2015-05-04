16bf75666b966279a89606f3e7596da9c35e3d32
davegson <dave.gson@gmail.com>
2015-05-04 15:01:36 +0200

    getting the dates of an offer now possible with date range

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -7,8 +7,16 @@ class Offer < ActiveRecord::Base
 	validates :every, inclusion: { in: %w(week day) }
 	validates_presence_of :every, :start_date, :end_date, :dish
 	
-	def dates(options={})
-		Recurrence.new({:every => every, :on => on, :interval => interval, :repeat => repeat, :starts => start_date, :until => end_date}.merge(options)).events
 	scope :in_range, ->(range) { where("end_date >= ?", range.first).where("start_date <= ?", range.last) }
+	
+	def dates(range = nil, options={})
+		recurrence_params = {:every => every, :on => on, :interval => interval, :repeat => repeat, :starts => start_date, :until => end_date}.merge(options)
+		
+		if range
+			Recurrence.new(recurrence_params.merge(:starts => range.first, :until => range.last)).events
+		else
+			Recurrence.new(recurrence_params).events
+		end
+	end
 	end
 end
