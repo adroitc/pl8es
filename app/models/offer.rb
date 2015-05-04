42c2f6aae794a6689cfc7995b1aa63cd4fe25c64
davegson <dave.gson@gmail.com>
2015-05-04 15:04:16 +0200

    added a method that returns offers on the different dates of a time range

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -18,5 +18,35 @@ class Offer < ActiveRecord::Base
 			Recurrence.new(recurrence_params).events
 		end
 	end
+	
+	def self.offers_with_dates(offers, range)
+		# firstly converts the offers to this format:
+		# => [ { "dish_id" => 1, "dates" => [ Mon, 13 Apr 2015, Wed, 15 Apr 2015, Fri, 17 Apr 2015 ] },
+		#      { "dish_id" => 2, "dates" => [ Mon, 13 Apr 2015, Tue, 14 Apr 2015 ] } ]
+		
+		offer_arr = Array.new(
+			offers.map do |o|
+			 { "dish_id" => o.dish_id, "dates" => o.dates(range) }
+			end
+		)
+		
+		# further convert them to the desired format:
+		# => { 
+		#      Mon, 13 Apr 2015 => [1],
+		#      Tue, 14 Apr 2015 => [1, 2],
+		#      Wed, 15 Apr 2015 => [1],
+		#      .
+		#      .
+		#      .
+		#    }
+		
+		final_hash = {}
+		range.each do |date|
+			# select the ones who include the desired date -- # get the dish_ids of these elements
+			dish_ids = offer_arr.select { |h| h["dates"].include?(date.to_date) }.map { |h| h["dish_id"] }
+			final_hash.store(date, dish_ids)
+		end
+		
+		return final_hash
 	end
 end
