4da1ba5863f140b7c5bbd5f93bf6171809dae42f
davegson <dave.gson@gmail.com>
2015-05-05 18:13:41 +0200

    simplified offers#index
    more DRY attempt, minimal, but simpler

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,8 @@
 class OffersController < ApplicationController
 	
 	def index
-		week_start = (Date.today + params[:weeks].to_i*7).at_beginning_of_week
-		week_end = (Date.today + params[:weeks].to_i*7).at_end_of_week
-		
-		week_range = week_start..week_end
+		selected_date = Date.today + params[:weeks].to_i*7
+		week_range = selected_date.at_beginning_of_week..selected_date.at_end_of_week
 		
 		@offers = current_user.restaurant.offers.in_range(week_range)
 		@dates_with_offers = Offer.dates_with_offers(@offers, week_range)
