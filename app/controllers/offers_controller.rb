2ee726a7dea811aaa39052f7123178bccfabff7f
davegson <dave.gson@gmail.com>
2015-05-05 13:05:50 +0200

    added offers index action
    based on the old view, now renders the correct dishes on the fitting
    weekdays.
    
    Also cleaner HTML/View with helper

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,9 @@
 class OffersController < ApplicationController
 	
 	def index
+		week_range = Date.today.at_beginning_of_week..Date.today.at_end_of_week
+		
+		@offers = current_user.restaurant.offers.in_range(week_range)
+		@dates_with_offers = Offer.dates_with_offers(@offers, week_range)
 	end
 end
--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,2 +1,5 @@
 module OffersHelper
+	def output_dates_range(dates)
+		l(dates.to_a.first[0], format: :long) + " - " + l(dates.to_a.last[0], format: :long)
+	end
 end
