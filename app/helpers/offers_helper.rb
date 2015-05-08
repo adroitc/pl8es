bc5ebc81b78df89977369fb5cb5443250db4119e
davegson <dave.gson@gmail.com>
2015-05-08 17:36:16 +0200

    adjusted offer form - better usability

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -2,4 +2,13 @@ module OffersHelper
 	def output_dates_range(dates)
 		l(dates.to_a.first[0], format: :long) + " - " + l(dates.to_a.last[0], format: :long)
 	end
+	
+	def weekdays_array
+		final_arr = []
+		[:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday].each do |weekday|
+			final_arr << [weekday.to_s.titleize, weekday]
+		end
+		
+		return final_arr
+	end
 end
