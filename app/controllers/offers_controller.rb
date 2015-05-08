d91494085cbcb487ca3cd6ba5b0e4d9384efd99a
davegson <dave.gson@gmail.com>
2015-05-08 17:36:33 +0200

    added basic create action

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -11,4 +11,19 @@ class OffersController < ApplicationController
 	def new
 		@offer = Offer.new
 	end
+	
+	def create
+		@offer = current_user.restaurant.offers.build(offer_params)
+		
+		unless @offer.save
+			render :new
+		end
+	end
+	
+	private
+		
+		def offer_params
+			params[:offer][:on].shift if params[:offer][:on].first.blank? # strip the empty element from the hidden field - this is used to send data even when no days are selected (
+			params.require(:offer).permit(:dish_id, :start_date, :end_date, :interval, :every, :on => [])
+		end
 end
